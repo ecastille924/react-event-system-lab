93b736e1a7069cd69e6982a4de9b1405e2350b48
Erick Castille <ecastille924@gmail.com>
2021-08-22 19:26:47 -0400

    commit tree a729dd7be1898dcd34ea7e8db126c90e0a4fde4a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+    
+    focusing = () => {
+        console.log('Good!')
+    }
+    
+    blurring = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    render() {
+        return (
+            <button onFocus={this.focusing} onBlur={this.blurring}>button</button>
+        )
+    }
+}
+
 // Code EyesOnMe Component Here
